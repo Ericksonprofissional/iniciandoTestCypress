e5ff7c1378eac77b6f67242c6aa690c74d3b53a4
ericksonprofissional <ericksonprofissional@gmail.com>
2021-01-25 20:13:50 -0400

    Teste Each

--- cypress/integration/dinamic.spec.js
+++ cypress/integration/dinamic.spec.js
@@ -26,4 +26,50 @@ describe('Dinamic test', () => {
                 });
         });
     });
+
+    it('Selec all', function () {
+        cy.fixture('useData')
+        .as('usuario')
+        .then(() => {
+            cy.get('#formNome').type(this.usuario.nome);
+            cy.get('#formSobrenome').type(this.usuario.sobreNome);
+            cy.get(`[name=formSexo][value=${this.usuario.sexo}]`).click();
+            cy.get(`[name=formComidaFavorita]`).click({multiple: true});
+            cy.get('#formEscolaridade').select(this.usuario.escolaridade)
+            cy.get('#formEsportes').select(this.usuario.esportes)
+            cy.clickAlert('#formCadastrar', 'Tem certeza que voce eh vegetariano?')
+        });
+    })
+
+    it('Selec all with EACH', function () {
+        cy.fixture('useData')
+        .as('usuario')
+        .then(() => {
+            cy.get('#formNome').type(this.usuario.nome);
+            cy.get('#formSobrenome').type(this.usuario.sobreNome);
+            cy.get(`[name=formSexo][value=${this.usuario.sexo}]`).click();
+            cy.get(`[name=formComidaFavorita]`).each($el => cy.wrap($el).click());
+            cy.get('#formEscolaridade').select(this.usuario.escolaridade)
+            cy.get('#formEsportes').select(this.usuario.esportes)
+            cy.clickAlert('#formCadastrar', 'Tem certeza que voce eh vegetariano?')
+        });
+    })
+
+    it('Selec with EACH menos Vegetario', function () {
+        cy.fixture('useData')
+        .as('usuario')
+        .then(() => {
+            cy.get('#formNome').type(this.usuario.nome);
+            cy.get('#formSobrenome').type(this.usuario.sobreNome);
+            cy.get(`[name=formSexo][value=${this.usuario.sexo}]`).click();
+            cy.get(`[name=formComidaFavorita]`).each($el => {
+                if($el.val() != 'vegetariano')
+                    cy.wrap($el).click();
+            });
+            cy.get('#formEscolaridade').select(this.usuario.escolaridade);
+            cy.get('#formEsportes').select(this.usuario.esportes);
+            cy.get('#formCadastrar').click();
+            cy.get('#resultado > :nth-child(1)').should('contain','Cadastrado');
+        });
+    })
 });
