e0298828472d39d1d7537714674bd69a418907fd
ericksonprofissional <ericksonprofissional@gmail.com>
2021-01-26 20:55:23 -0400

    Alterando conta

--- cypress/integration/barriga/login.spec.js
+++ cypress/integration/barriga/login.spec.js
@@ -5,12 +5,21 @@ describe('Dinamic test', () => {
         cy.visit('http://barrigareact.wcaquino.me/');
         cy.barrigaLogin();
     });
+    const uid = Cypress._.random(0, 1e6);
+    console.log(uid)
 
     it('Should create an account',() => {
         cy.get('[data-test=menu-settings]').click();
         cy.get('[href="/contas"]').click();
-        cy.get('[data-test=nome]').type('Aline1');
+        cy.get('[data-test=nome]', {timeout:5000}).type(`Aline${uid}`);
         cy.get('.btn').click();
         cy.alert('.toast-message', 'Conta inserida com sucesso');
+    });
+
+    it('Should alter an account', () => {
+        cy.xpath(`//table//td[contains(., 'Aline${uid}')]/..//i[@class='far fa-edit']`).click();
+        cy.get('[data-test=nome]', {timeout:5000}).type(` Martinez`);
+        cy.get('.btn').click();
+        cy.alert('.toast-message', 'Conta atualizada com sucesso');
     });
 });
